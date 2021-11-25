743fdce0a8e4f58dbe5fee86547c0e7b05e0b01e
sujithshajee <Subith@12345>
2021-11-24 20:54:18 -0500

    automation script and readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Automation_Project
+
+## Description
+The bash script is used to perform below activities on the Ubuntu server.
+
+```bash
+Updates the package information
+Ensures that HTTP Apache server is installed
+Ensures that HTTP Apache server is running
+Ensures that HTTP Apache service is enabled
+Archiving HTTP Server logs from /var/log/apache2/ to S3
+```
+
+## Prerequisites
+Ensure below requirements fulfilled on server
+
+```bash
+  Ubuntu Server 18.04 LTS (HVM)
+  Bash configured
+  AWS CLI configured - Refer link for details https://linuxhint.com/install_aws_cli_ubuntu/ 
+```
+
+
+## Usage
+
+```bash
+Clone the repo on server
+Update below variables with appropriate values by editing automation.sh
+  #update the variable with the s3 bucket name in aws
+  s3_bucket="upgrad-sujithshajee"
+  #provide the prefix that needs to be used to archive file on s3 bucket
+  fileprefix="sujith"
+Provide execute permission on the automation.sh file running below command
+  sudo su
+  chmod 755 automation.sh
+Execute the script using following command
+  ./automation.sh
+```
---
+++ automation.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+##Initialize variables
+#update the variable with the s3 bucket name in aws
+s3_bucket="upgrad-sujithshajee"
+#provide the prefix that needs to be used to archive file on s3 bucket
+fileprefix="sujith"
+
+# Download package information from all configured sources
+apt update -y
+
+# Check if apache http server is installed. Install if not installed already
+installstat=$(dpkg --get-selections | grep apache2)
+
+if [[ $installstat == *"apache2"* ]]; then
+    echo "Apache process is installed..."
+else
+    echo "Apache process is not installed. Hence installing the service..."
+    apt install apache2 -y
+fi
+
+# Check if apache http server is running. Start server if not running already
+servstat=$(systemctl status apache2)
+
+if [[ $servstat == *"active (running)"* ]]; then
+    echo "Apache process is running..."
+else
+    echo "Apache process is not running. Hence starting the service..."
+    systemctl start apache2
+fi
+
+# Check if apache http server is enabled. Start server if not enabled already
+enablestat=$(systemctl --all list-unit-files --type=service | grep -i "apache2")
+
+if [[ $enablestat == *"enabled"* ]]; then
+    echo "Apache process is enabled..."
+else
+    echo "Apache process is not enabled. Hence enabling the service..."
+    systemctl enable apache2
+fi
+
+# Check if aws cli is installed. Install if not installed already
+awscliinstallstat=$(dpkg --get-selections | grep -i "awscli")
+if [[ $awscliinstallstat == *"awscli"* ]]; then
+    echo "AWS cli is installed..."
+else
+    echo "AWS cli is not installed. Hence installing the service..."
+    apt install awscli -y
+fi
+
+
+#check if s3 bucket indicated exists. If not exit indicating error
+s3bucketstat=$(aws s3 ls | grep -i "$s3bucket")
+if [[ $s3bucketstat == *"$s3bucket"*  ]]; then
+    echo "AWS cli is configured and can access s3 bucket..."
+else
+    echo "AWS cli is not configured or s3 bucket name is incorrect. Log Archival will not proceed further..."
+    exit 1
+fi
+
+DIR="/var/log/apache2/"
+if [[ -d "$DIR" ]]; then
+    echo "Archiving log files in ${DIR}..."
+    timestamp=$(date '+%d%m%Y-%H%M%S')
+    filename=$fileprefix"-httpd-logs-"$timestamp
+    find $DIR -name "*.log" | tar -zcvf /tmp/$filename.tar  -P  -T -
+    gzip /tmp/$filename.tar
+    aws s3 cp /tmp/$filename.tar.gz s3://${s3_bucket}/${filename}.tar.gz
+else
+    echo "Directory path ${DIR} not found..."
+    exit 1
+fi
+
+exit 0
